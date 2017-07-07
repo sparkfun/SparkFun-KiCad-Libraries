a48b9160544711b4be87b0a04667d068ed6cd035
marshalltaylorSFE <marshall.taylor@sparkfun.com>
2017-07-07 16:46:42 -0600

    Organized silicon footprints, started mask restrict script

---
+++ Tools/SparkFun-Eagle-Libraries_Conversion/Python/Add4thouSilkRestrict.bat
@@ -0,0 +1,1 @@
+python ConvertModLineMultiPoly.py C:\github\SparkFun-KiCad-Libraries\Footprints\SparkFun.pretty C:\github\SparkFun-KiCad-Libraries\Tools\SparkFun-Eagle-Libraries_Conversion\Export.pretty OSHW-LOGO-L
