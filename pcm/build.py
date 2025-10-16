9934ff0d76f2b3115bad6914ad02ea8fb8519794
PaulZC <pc235603@gmail.com>
2025-10-16 10:32:50 +0100

    Add build and workflow

---
+++ pcm/build.py
@@ -0,0 +1,145 @@
+
+import os
+from os import path
+import shutil
+import pathlib
+import json
+import sys
+import fnmatch
+
+src_path = path.join(path.dirname(__file__),'..')
+version_path = path.join(path.dirname(__file__),'_version.py')
+metadata_template = path.join(path.dirname(__file__),'metadata_template.json')
+resources_path = path.join(path.dirname(__file__),'resources')
+build_path = path.join('build')
+
+# https://stackoverflow.com/a/6257321
+def findReplace(directory, find, replace, filePattern):
+    for path, dirs, files in os.walk(os.path.abspath(directory)):
+        for filename in fnmatch.filter(files, filePattern):
+            filepath = os.path.join(path, filename)
+            with open(filepath) as f:
+                s = f.read()
+            s = s.replace(find, replace)
+            with open(filepath, "w") as f:
+                f.write(s)
+
+# ======================================================
+
+# Delete build and recreate
+try:
+    shutil.rmtree(build_path)
+except FileNotFoundError:
+    pass
+os.mkdir(build_path)
+os.mkdir(path.join(build_path,'content'))
+os.chdir(build_path)
+
+# Copy symbols, footprints and 3dmodels
+shutil.copytree(path.join(src_path,'symbols'), path.join('content'))
+shutil.copytree(path.join(src_path,'footprints'), path.join('content'))
+shutil.copytree(path.join(src_path,'3dmodels'), path.join('content'))
+
+# Replace every occurrence of "(model "${SPARKFUN_KICAD_LIBRARY}/3dmodels/3d-library.3dshapes/"
+# with "(model "${KICAD9_3RD_PARTY}/3dmodels/com_github_sparkfun_sparkfun-kicad-libraries/3d-library.3dshapes/"
+findReplace(path.join('content'), '(model "${SPARKFUN_KICAD_LIBRARY}/3dmodels/3d-library.3dshapes/',
+    '(model "${KICAD9_3RD_PARTY}/3dmodels/com_github_sparkfun_SparkFun-KiCad-Libraries/3d-library.3dshapes/', '*.*')
+
+# This is the with-PCM_ version
+# Replace every occurrence of "(symbol "SparkFun-"
+# with "(symbol "PCM_SparkFun-"
+findReplace(path.join('content'), "(symbol \"SparkFun-",
+    "(symbol \"PCM_SparkFun-", "*.*")
+
+# Copy icon
+shutil.copytree(resources_path, path.join('content','resources'))
+
+# Copy metadata
+metadata = path.join('content','metadata.json')
+shutil.copy(metadata_template, metadata)
+
+# Load up json script
+with open(metadata) as f:
+    md = json.load(f)
+
+# Get version from _version.py
+# https://stackoverflow.com/a/7071358
+import re
+verstrline = open(version_path, "rt").read()
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, verstrline, re.M)
+if mo:
+    verstr = mo.group(1)
+else:
+    verstr = "9.0.0"
+
+# Update download URL
+md['versions'][0].update({
+    'version': verstr,
+    'download_url': 'https://github.com/sparkfun/SparkFun-KiCad-Libraries/releases/download/v{0}/SparkFun-KiCad-Libraries-{0}-with-pcm.zip'.format(verstr)
+})
+
+# Update metadata.json
+with open(metadata, 'w') as of:
+    json.dump(md, of, indent=2)
+
+# Zip all files
+zip_file = 'SparkFun-KiCad-Libraries-{0}-with-pcm.zip'.format(md['versions'][0]['version'])
+shutil.make_archive(pathlib.Path(zip_file).stem, 'zip', 'content')
+
+# Rename the content directory so we can upload it as an artifact - and avoid the double-zip
+shutil.move('content', 'SparkFun-KiCad-Libraries-{0}-with-pcm'.format(md['versions'][0]['version']))
+
+# ======================================================
+
+# Do it all again for the without-PCM_ version
+os.mkdir(path.join('content'))
+
+# Copy symbols, footprints and 3dmodels
+shutil.copytree(path.join(src_path,'symbols'), path.join('content'))
+shutil.copytree(path.join(src_path,'footprints'), path.join('content'))
+shutil.copytree(path.join(src_path,'3dmodels'), path.join('content'))
+
+# Replace every occurrence of "(model "${SPARKFUN_KICAD_LIBRARY}/3dmodels/3d-library.3dshapes/"
+# with "(model "${KICAD9_3RD_PARTY}/3dmodels/com_github_sparkfun_sparkfun-kicad-libraries/3d-library.3dshapes/"
+findReplace(path.join('content'), '(model "${SPARKFUN_KICAD_LIBRARY}/3dmodels/3d-library.3dshapes/',
+    '(model "${KICAD9_3RD_PARTY}/3dmodels/com_github_sparkfun_SparkFun-KiCad-Libraries/3d-library.3dshapes/', '*.*')
+
+# Copy icon
+shutil.copytree(resources_path, path.join('content','resources'))
+
+# Copy metadata
+metadata = path.join('content','metadata.json')
+shutil.copy(metadata_template, metadata)
+
+# Load up json script
+with open(metadata) as f:
+    md = json.load(f)
+
+# Get version from _version.py
+# https://stackoverflow.com/a/7071358
+import re
+verstrline = open(version_path, "rt").read()
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, verstrline, re.M)
+if mo:
+    verstr = mo.group(1)
+else:
+    verstr = "9.0.0"
+
+# Update download URL
+md['versions'][0].update({
+    'version': verstr,
+    'download_url': 'https://github.com/sparkfun/SparkFun-KiCad-Libraries/releases/download/v{0}/SparkFun-KiCad-Libraries-{0}-without-pcm.zip'.format(verstr)
+})
+
+# Update metadata.json
+with open(metadata, 'w') as of:
+    json.dump(md, of, indent=2)
+
+# Zip all files
+zip_file = 'SparkFun-KiCad-Libraries-{0}-without-pcm.zip'.format(md['versions'][0]['version'])
+shutil.make_archive(pathlib.Path(zip_file).stem, 'zip', 'content')
+
+# Rename the content directory so we can upload it as an artifact - and avoid the double-zip
+shutil.move('content', 'SparkFun-KiCad-Libraries-{0}-without-pcm'.format(md['versions'][0]['version']))
